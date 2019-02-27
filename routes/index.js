1692de970f9c2d74c56eacb6142334ab59989447
Phan Quang <phanlequang99@gmail.com>
2019-02-27 20:02:24 +0700

    Init commit

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var cheerio = require('cheerio');
+var request = require('request');
+var datetime = require('node-datetime');
+ 
+/* GET home page. */
+router.get('/', function (req, res) {
+  res.render('index');
+});
+function sortProduct(listProducts,desc){
+ return listProducts.sort((a, b) =>{
+if(!desc){
+  return parseFloat(b.discount_percent) - parseFloat(a.discount_percent)
+}
+else{
+  return parseFloat(a.discount_percent) - parseFloat(b.discount_percent)
+}
+ });
+   
+}
+router.get('/deal', (req, res) => {
+   link = 'https://tiki.vn/api/v2/deals/collections/?type=now&page=1&per_page=100&from=1548614905&to=1553798905';
+  request(link, function (err, result, body) {
+    let finalResult = {};
+    let reponse = [];
+    let data = JSON.parse(body);
+
+let sortData=sortProduct(data.data,false)
+
+
+    sortData.forEach(element => {
+      if (element.discount_percent) {
+        reponse.push(element);
+      }
+    });
+    res.json(reponse);
+  })
+})
+
+router.get('/time', function (req, res) {
+  var dt = datetime.create('2015-04-30');
+ var date=new Date();
+var formattedDate = dt.format('d/m/y');
+console.log(dt.getTime()<date.getTime())
+ 
+});
+module.exports = router;
