508c1490f09fce5adc451fe447f2eed644848b8e
Phan Quang <phanlequang99@gmail.com>
2019-02-28 00:48:25 +0700

    integration front-end and back-end

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var datetime = require('node-datetime');
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index');
+  res.render('index',{data:''});
 });
 function sortProduct(listProducts, desc) {
   return listProducts.sort((a, b) => {
@@ -27,8 +27,18 @@ function getDataProducts(perPage,from,to,callback) {
     callback(data);
   });
 }
-router.get('/getDeal', (req, res) => {
-  getDataProducts(100,1548614905,1553798905,(data) => {
+router.post('/', (req, res) => {
+  let perPage=20;
+  // let from=1548614905
+  // let to=1553798905;
+  console.log('req'+req.body.from)
+  var from = datetime.create(req.body.from.toString()).getTime()/1000;
+  var to=datetime.create(req.body.to.toString()).getTime()/1000;
+ console.log('from'+from);
+  
+  console.log('From' +req.body.from)
+  console.log('To' +req.body.to)
+  getDataProducts(perPage,from,to,(data) => {
     let reponse = [];
     let sortData = sortProduct(data.data, false)
     sortData.forEach(element => {
@@ -36,20 +46,27 @@ router.get('/getDeal', (req, res) => {
         reponse.push(element);
       }
     });
-    res.json(reponse);
+ 
+    res.render('index',{data:reponse})
+  
+   
   })
 
 
 
 
 })
+router.post('/getList',(req,res)=>{
+
+});
 
 
 router.get('/time', function (req, res) {
-  var dt = datetime.create('2015-04-30');
-  var date = new Date();
-  var formattedDate = dt.format('d/m/y');
-  console.log(dt.getTime() < date.getTime())
-
+  var dt = datetime.create('02/06/2019');
+  var dt2=datetime.create('02/10/2019')
+ 
+  
+console.log(dt.getTime()/1000);
+console.log(dt2.getTime()/1000);
 });
 module.exports = router;
