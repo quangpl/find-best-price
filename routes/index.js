9562bb53bd16a71b43453d785a9687dccff7fa89
Phan Quang <phanlequang99@gmail.com>
2019-02-27 22:34:50 +0700

    Fix front-end

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,34 @@ var router = express.Router();
 var cheerio = require('cheerio');
 var request = require('request');
 var datetime = require('node-datetime');
- 
+
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render('index');
 });
-function sortProduct(listProducts,desc){
- return listProducts.sort((a, b) =>{
-if(!desc){
-  return parseFloat(b.discount_percent) - parseFloat(a.discount_percent)
+function sortProduct(listProducts, desc) {
+  return listProducts.sort((a, b) => {
+    if (!desc) {
+      return parseFloat(b.discount_percent) - parseFloat(a.discount_percent)
+    }
+    else {
+      return parseFloat(a.discount_percent) - parseFloat(b.discount_percent)
+    }
+  });
+
 }
-else{
-  return parseFloat(a.discount_percent) - parseFloat(b.discount_percent)
-}
- });
-   
-}
-router.get('/deal', (req, res) => {
-   link = 'https://tiki.vn/api/v2/deals/collections/?type=now&page=1&per_page=100&from=1548614905&to=1553798905';
+function getDataProducts(perPage,from,to,callback) {
+  var link = 'https://tiki.vn/api/v2/deals/collections/?type=now&page=1&per_page='+perPage+'&from='+from+'&to='+to;
+  console.log(link);
   request(link, function (err, result, body) {
-    let finalResult = {};
-    let reponse = [];
     let data = JSON.parse(body);
-
-let sortData=sortProduct(data.data,false)
-
-
+    callback(data);
+  });
+}
+router.get('/getDeal', (req, res) => {
+  getDataProducts(100,1548614905,1553798905,(data) => {
+    let reponse = [];
+    let sortData = sortProduct(data.data, false)
     sortData.forEach(element => {
       if (element.discount_percent) {
         reponse.push(element);
@@ -36,13 +38,18 @@ let sortData=sortProduct(data.data,false)
     });
     res.json(reponse);
   })
+
+
+
+
 })
+
 
 router.get('/time', function (req, res) {
   var dt = datetime.create('2015-04-30');
- var date=new Date();
-var formattedDate = dt.format('d/m/y');
-console.log(dt.getTime()<date.getTime())
- 
+  var date = new Date();
+  var formattedDate = dt.format('d/m/y');
+  console.log(dt.getTime() < date.getTime())
+
 });
 module.exports = router;
