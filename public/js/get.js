35f106c79321e69663c9169ef66c9efde0804c3e
Phan Quang <phanlequang99@gmail.com>
2019-03-02 23:33:45 +0700

    Fix F-B

---
+++ public/js/get.js
@@ -0,0 +1,25 @@
+// $(document).ready(function()
+// { 
+     
+// var submit=$("#find-button");
+// submit.click(()=>{
+//     $.ajax({
+//         method: "POST",
+//         url: "/getList",
+//         data: {
+//             from: $('input[id="from"]').val(),
+//             to:  $('input[id="to"]').val()
+//         },
+//         success: function(data) {
+//            console.log(data);
+//           }
+//       })
+     
+        
+// })
+// });
+// // $(document).ready(function(){
+// //     $('#find-button').on('click',()=>{
+// //         console.log('hi');
+// //     })
+// // })
--- routes/index.js
+++ routes/index.js
@@ -1,72 +1,91 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var cheerio = require('cheerio');
-var request = require('request');
-var datetime = require('node-datetime');
+var cheerio = require("cheerio");
+var request = require("request");
+var datetime = require("node-datetime");
 
 /* GET home page. */
-router.get('/', function (req, res) {
-  res.render('index',{data:''});
-});
-function sortProduct(listProducts, desc) {
-  return listProducts.sort((a, b) => {
-    if (!desc) {
-      return parseFloat(b.discount_percent) - parseFloat(a.discount_percent)
-    }
-    else {
-      return parseFloat(a.discount_percent) - parseFloat(b.discount_percent)
-    }
-  });
+router.get("/", function(req, res) {
+  let date=new Date();
+  var day=date.getDate();
+  var month=date.getMonth();
+  var year=date.getFullYear();
 
-}
-function getDataProducts(perPage,from,to,callback) {
-  var link = 'https://tiki.vn/api/v2/deals/collections/?type=now&page=1&per_page='+perPage+'&from='+from+'&to='+to;
-  console.log(link);
-  request(link, function (err, result, body) {
-    let data = JSON.parse(body);
-    callback(data);
-  });
-}
-router.post('/getList', (req, res) => {
-  let perPage=20;
-  // let from=1548614905
-  // let to=1553798905;
-  console.log('req'+req.body.from)
-  var from = datetime.create(req.body.from.toString()).getTime()/1000;
-  var to=datetime.create(req.body.to.toString()).getTime()/1000;
- console.log('from'+from);
-  
-  console.log('From' +req.body.from)
-  console.log('To' +req.body.to)
-  getDataProducts(perPage,from,to,(data) => {
+  var dateFrom=datetime.create(createDate(day,month+1,year)).getTime()/1000;
+  var dateTo=datetime.create(createDate(day,month+2,year)).getTime()/1000;
+
+  getDataProducts(20, dateFrom, dateTo, data => {
     let reponse = [];
-    let sortData = sortProduct(data.data, false)
+    let sortData = sortProduct(data.data, false);
     sortData.forEach(element => {
       if (element.discount_percent) {
         reponse.push(element);
       }
     });
- 
-    res.render('index',{data:reponse})
-  
-   
-  })
 
-
-
-
-})
-router.post('/getList',(req,res)=>{
-
+    res.render("index", { data: reponse ,error:''});
+  });
 });
-
-
-router.get('/time', function (req, res) {
-  var dt = datetime.create('02/06/2019');
-  var dt2=datetime.create('02/10/2019')
+function sortProduct(listProducts, desc) {
+  return listProducts.sort((a, b) => {
+    if (!desc) {
+      return parseFloat(b.discount_percent) - parseFloat(a.discount_percent);
+    } else {
+      return parseFloat(a.discount_percent) - parseFloat(b.discount_percent);
+    }
+  });
+}
+function getDataProducts(perPage, from, to, callback) {
+  var link =
+    "https://tiki.vn/api/v2/deals/collections/?type=now&page=1&per_page=" +
+    perPage +
+    "&from=" +
+    from +
+    "&to=" +
+    to;
+  console.log(link);
+  request(link, function(err, result, body) {
+    let data = JSON.parse(body);
+    callback(data);
+  });
+}
+router.post("/getList", (req, res) => {
+  let perPage = req.body.count;
+  console.log("From1" + req.body.from);
+  console.log("To1" + req.body.to);
  
+  var from = datetime.create(req.body.from.toString()).getTime() / 1000;
+  var to = datetime.create(req.body.to.toString()).getTime() / 1000;
+
+let error;
+  getDataProducts(perPage, from, to, data => {
+    let reponse = [];
+    let sortData = sortProduct(data.data, false);
+    sortData.forEach(element => {
+      if (element.discount_percent) {
+        reponse.push(element);
+      }
+    });
+if(data.data.length==0) {error= 'Nhập sai ngày (Ngày kết thúc phải lớn hơn ngày bắt đầu và lớn hơn ngày hiện tại'};
+ 
+   res.render("index", { data: reponse,error:error });
+//   res.end('ok');
+  });
+});
+ 
+function createDate(m,d,y){
+return d+'/'+m+'/'+y;
+}
+router.get("/time", function(req, res) {
   
-console.log(dt.getTime()/1000);
-console.log(dt2.getTime()/1000);
+
+  var d = new Date();
+  console.log(d.getDay() + "/" + d.getMonth() + 1 + "/" + d.getFullYear());
+  var dt2 = datetime.create(d);
+  console.log("dd" + dt2.getTime());
+});
+router.get("/getList", function(req, res) {
+  
+res.redirect('/');
 });
 module.exports = router;
